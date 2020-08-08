9b089a48ba5fcc658b9bab57a50bc256bf4d5eb8
trimethylpentan <pascal.0399@yahoo.de>
2020-08-08 19:11:03 +0200

    Routing Problems

--- src/App.js
+++ src/App.js
@@ -1,38 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from "./Components/Navbars/Navbar";
-import SideNav from "./Components/Navbars/SideNav";
 
 function App() {
-  return (
-    <>
-      <Navbar/>
-      <div id="layoutSidenav">
-        <SideNav/>
-        <div id="layoutSidenav_content">
-          <main>
-            {renderApp()}
-          </main>
-          <footer className="py-4 bg-light mt-auto">
-            <div className="container-fluid">
-              <div className="d-flex align-items-center justify-content-between small">
-                <div className="text-muted">Copyright &copy; Your Website 2020</div>
-                <div>
-                  <a href="#">Privacy Policy</a>
-                  &middot;
-                  <a href="#">Terms &amp; Conditions</a>
-                </div>
-              </div>
-            </div>
-          </footer>
-        </div>
-      </div>
-    </>
-  );
-}
-
-function renderApp() {
   return <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo"/>
--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,18 +1,19 @@
 import React from "react";
-import {Route, Router} from "react-router-dom";
+import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
 import { createBrowserHistory } from 'history';
 import App from "./App";
 import ListServices from "./ListServices";
 
 function AppRouter() {
-  console.log(createBrowserHistory());
   return <Router history={createBrowserHistory()}>
-    <Route path={"/"}>
-      <App/>
-    </Route>
-    <Route path={"/services/list"}>
-      <ListServices/>
-    </Route>
+    <Switch>
+      <Route path={"/"}>
+        <App/>
+      </Route>
+      <Route path={"/services/list"}>
+        <ListServices/>
+      </Route>
+    </Switch>
   </Router>
 }
 
